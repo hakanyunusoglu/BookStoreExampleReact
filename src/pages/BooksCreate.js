3240f69820227171332e5f4a9fea399a966153cb
hakanyunusoglu93@gmail.com <hakanyunusoglu93@gmail.com>
2022-04-05 14:39:41 +0300

    fgvdgdgdgd

--- src/pages/BooksCreate.js
+++ src/pages/BooksCreate.js
@@ -1,26 +1,37 @@
 import React, { useState } from 'react'
 import { Stack,TextField,PrimaryButton } from '@fluentui/react'
-import { HandRight16Filled } from '@fluentui/react-icons'
+import axios from 'axios'
 
 export default function BooksCreate() {
   const [pageData,setPageData] = useState({
-    "id": "",
     "name": "",
     "author": "",
     "imgUrl": "",
     "about": ""
   })
+  const onChangeText = (e)=>{
+  setPageData({...pageData,[e.target.name]:e.target.value})
+  }
+  function createBook()
+  {
+    axios.post("https://api-bookseller.herokuapp.com/books",pageData).then(response=>{
+      if(response.status === 201){
+      alert("Book Created Successfully!");
+      }else{
+        alert("Book Created Failure!");
+      }
+    })
+  }
   return (
     <div style={{width:"100%",paddingLeft:50}}>
     <div className='content'>
     <div className='content-header'>Books Create</div>
     <Stack tokens={{childrenGap:20}} styles={{root:{width:"50%", marginLeft:10, marginTop:10}}}>
-     <TextField label='ID' name='id' value={pageData.id} placeholder='Please enter ID here'/>
-      <TextField label='Name' name='name' value={pageData.name} placeholder='Please enter name here'/>
-      <TextField label='Img' name='imgUrl' value={pageData.imgUrl} placeholder='Please enter img url here'/>
-      <TextField label='Author' name='author' value={pageData.author} placeholder='Please enter author here'/>
-      <TextField label='About' name='about' value={pageData.about} placeholder='Please enter about here'/>
-      <PrimaryButton text='Create Book' style={{width:120,height:40}}/>
+      <TextField label='Name' name='name' value={pageData.name} onChange={onChangeText} placeholder='Please enter name here'/>
+      <TextField label='Img' name='imgUrl' value={pageData.imgUrl} onChange={onChangeText} placeholder='Please enter img url here'/>
+      <TextField label='Author' name='author' value={pageData.author} onChange={onChangeText} placeholder='Please enter author here'/>
+      <TextField label='About' name='about' value={pageData.about} onChange={onChangeText} placeholder='Please enter about here'/>
+      <PrimaryButton text='Create Book' style={{width:120,height:40}} onClick={()=>createBook()}/>
     </Stack>
     </div>
     </div>
--- src/pages/BooksEdit.js
+++ src/pages/BooksEdit.js
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Stack,TextField,PrimaryButton } from '@fluentui/react'
+import axios from 'axios'
+import { useLocation } from 'react-router-dom'
 
 
 export default function BooksEdit() {
+  const location = useLocation();
+  const [pageData,setPageData] = useState({
+    "name": "",
+    "author": "",
+    "imgUrl": "",
+    "about": ""
+  })
+  const onChangeText = (e)=>{
+  setPageData({...pageData,[e.target.name]:e.target.value})
+  }
+  function editBook()
+  {
+    axios.put("https://api-bookseller.herokuapp.com/books/"+location.state.id,pageData).then(response=>{
+      if(response.status === 200)
+      {
+        alert("Books are changed successfully!")
+      }else{
+        alert("Books are not changed!")
+      }
+    })
+  }
+  function fetchBookById(){
+    axios.get("https://api-bookseller.herokuapp.com/books/"+location.state.id).then(response=>setPageData(response.data));
+  }
+  useEffect(()=>{
+    fetchBookById();
+  },[])
   return (
-    <div style={{paddingTop:50}}>
-    
-   
-    
+    <div style={{width:"100%",paddingLeft:50}}>
+    <div className='content'>
+    <div className='content-header'>Books Edit</div>
+    <Stack tokens={{childrenGap:20}} styles={{root:{width:"50%", marginLeft:10, marginTop:10}}}>
+      <TextField label='Name' name='name' value={pageData.name} onChange={onChangeText} placeholder='Please enter name here'/>
+      <TextField label='Img' name='imgUrl' value={pageData.imgUrl} onChange={onChangeText} placeholder='Please enter img url here'/>
+      <TextField label='Author' name='author' value={pageData.author} onChange={onChangeText} placeholder='Please enter author here'/>
+      <TextField label='About' name='about' value={pageData.about} onChange={onChangeText} placeholder='Please enter about here'/>
+      <PrimaryButton text='Edit Book' style={{width:120,height:40}} onClick={()=>editBook()}/>
+    </Stack>
+    </div>
     </div>
   )
 }
